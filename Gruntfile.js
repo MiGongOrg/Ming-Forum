b8f0fed310f8234154d9fa52631bd831e092ab6b
Ming <muvoid@foxmail.com>
2016-10-13 18:35:35 +0800

    *修改并完善Grunt配置文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,23 +4,32 @@ module.exports = function (grunt) {
     //读取配置项
     pkg:grunt.file.readJSON("package.json"),
     //执行的具体任务
-
+    browserSync: {
+      bsFiles: {
+        src: ['./*.html','src/css/*.css','src/js/*.js']
+      },
+      options: {
+        server: {
+          baseDir: './'
+        }
+      }
+    },
     //压缩文件
     uglify:{
       // 加注释
       options:{
         banner:"/* <%= pkg.name %> <%= pkg.version %>*/ \n"
       },
-      buildOne:{
+      login:{
         //被压缩文件的路径
-        src: "src/js/jquery/jquery.js",
+        src: "src/js/login.js",
         //被压缩后的文件路径
-        dest: "dest/jquery.min.js"
+        dest: "dist/js/login.min.js"
       },
-      buildTwo:{
+      index:{
         files:{
           //将 config.js 压缩成 config.min.js
-          'dest/js/config.min.js' : 'src/js/config.js'
+          'dist/js/index.min.js' : 'src/js/index.js'
         }
       },
       buildAll:{
@@ -29,16 +38,16 @@ module.exports = function (grunt) {
           expand: true,
           cwd:'src/js',   //JS目录下
           src:'**/*.js',  //所有JS文件
-          dest:'dest/js'  //压缩到指定目录
+          dest:'dist/js'  //压缩到指定目录
         }]
       }
     },
-    //bower 在 bower.json 文件中配置 exportsOverride
+    // 提取文件 bower 在 bower.json 文件中配置 exportsOverride
     bower:{
       install:{
         options:{
-          targetDir: './assets',  //cmder 运行 grunt bower 时，会将项目文件提取出来放到设定的目录下
-          layout: 'byComponent'   //设置 byType 或 byComponent 会改变目录结构
+          targetDir: './assets',  // 运行 grunt x-bower 时，会将项目文件提取出来放到设定的目录下
+          layout: 'byComponent'   // 设置 byType 或 byComponent 会改变目录结构
         }
       }
     },
@@ -48,22 +57,25 @@ module.exports = function (grunt) {
           expand: true,
           cwd:'src/css',  //CSS目录下
           src:'**/*.css', //所有CSS文件
-          dest:'dest/css' //压缩到指定目录
+          dest:'dist/css' //压缩到指定目录
         }]
       }/*,
       combine:{
         files:{
-          'dest/css/combine.min.css' : ['src/css/main.css','src/css/index.css'] //需要注意先后顺序
+          'dist/css/combine.min.css' : ['src/css/main.css','src/css/index.css'] //需要注意先后顺序
         }
       }*/
     },
-    autoprefixer: {//添加 css 私有前缀
+    // 添加 css 私有前缀
+    autoprefixer: {
       options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9'] //浏览器兼容
+        browsers: ['last 2 versions', 'ie 8', 'ie 9'] // 浏览器兼容
       },
       css: {
         src:[
-        'src/css/login.css' //将哪个CSS文件添加浏览器私有前缀
+        // 将哪个CSS文件添加浏览器私有前缀
+        'src/css/login.css',
+        'src/css/index.css'
         ]
       }
     },
@@ -73,30 +85,35 @@ module.exports = function (grunt) {
           baseUrl: 'src/js',
           mainConfigFile: 'src/js/config.js', //requirejs 配置文件
           name: 'config',
-          out: 'dest/js/config.min.js'  //根据依赖关系压缩后输出的文件
+          out: 'dist/js/config.min.js'  //根据依赖关系压缩后输出的文件
         }
       }
     }
   });
-  //通过 load-grunt-tasks 加载任务（设定指定范围）
+  // 通过 load-grunt-tasks 加载任务（设定指定范围）
   require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
 
-  //加载任务（使用 load-grunt-tasks 后，以下这些可以省略）
-  //grunt.loadNpmTasks('grunt-bower-task');         //bower 文件提取插件
-  //grunt.loadNpmTasks('grunt-contrib-uglify');     //JS文件压缩插件
-  //grunt.loadNpmTasks('grunt-contrib-cssmin');     //css文件压缩插件
-  //grunt.loadNpmTasks('grunt-contrib-less');       //LESS 文件编译CSS
-  //grunt.loadNpmTasks('grunt-autoprefixer');       //添加CSS私有前缀
-  //grunt.loadNpmTasks('grunt-contrib-requirejs');  //加载 requirejs
+  // time grunt 任务运行所耗时间
+  require('time-grunt')(grunt);
   
-  //执行任务
-  grunt.registerTask('buAll', ['uglify:buildAll']);           //压缩所有JS文件
-  grunt.registerTask('cssMinAll', ['cssmin:minAll']);         //合并某个目录下的所有CSS文件
-  //grunt.registerTask('combineCss', ['cssmin:combine']);       //将某两个CSS文件合并成一个文件
-  grunt.registerTask('pageLess', ['less:page']);              //将LESS文件编译成 css文件
-  grunt.registerTask('autoprefixerCss',['autoprefixer:css']); //添加属性私有前缀
-  grunt.registerTask('requireJs',['requirejs:jsAll']);        //执行 requirejs
+  // 执行任务
+
+  // 免F5 刷新，文件监听
+  grunt.registerTask('x-sync', ['browserSync']);
+  // bower install 的文件提取到相应目录下
+  grunt.registerTask('x-bower', ['bower:install']);
+  // login.css 和 index.css 添加浏览器私有属性前缀
+  grunt.registerTask('x-autoprefixer',['autoprefixer:css']);
+  // 压缩某个目录下的所有 Css 文件到指定目录
+  grunt.registerTask('x-css', ['x-autoprefixer', 'cssmin:minAll']);
+  // login.js 和 index.js 压缩某个目录下的所有 JS 文件到指定目录
+  grunt.registerTask('x-js', ['uglify:login', 'uglify:index']);
+  // 将某两个CSS文件合并成一个文件   
+  // grunt.registerTask('combineCss', ['cssmin:combine']);
+  
+  //执行 requirejs
+  grunt.registerTask('requireJs',['requirejs:jsAll']);
 
   //默认执行任务(数组中依次执行默认任务)
-  grunt.registerTask('default', ['bower:install','autoprefixerCss']); //默认任务
+  grunt.registerTask('default', ['x-css', 'x-js', 'requireJs']);
 };
